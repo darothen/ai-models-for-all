9295235f616b1da680e2be4dc10ce3a5fece7642
darothen <daniel@danielrothenberg.com>
2023-11-25 09:18:05 -0700

    Re-factor to use container lifecycle hooks to manage model initialization

--- ai-models-modal/main.py
+++ ai-models-modal/main.py
@@ -2,6 +2,7 @@ import datetime
 import os
 import pathlib
 
+import modal
 from ai_models import model
 
 from . import config
@@ -21,7 +22,6 @@ logger = config.get_logger(__name__, add_handler=False)
 )
 def check_assets():
     import cdsapi
-    import modal
 
     logger.info(f"Running locally -> {modal.is_local()}")
 
@@ -54,12 +54,60 @@ def check_assets():
         logger.info(f"({i}) {asset}")
 
 
+@stub.cls(
+    secret=config.ENV_SECRETS,
+    gpu=config.DEFAULT_GPU_CONFIG,
+    network_file_systems={str(config.CACHE_DIR): volume},
+    concurrency_limit=1,
+    timeout=1_800,
+)
+class AIModel:
+    def __init__(
+        self,
+        # TODO: Re-factor arguments into a well-structured dataclass.
+        model_name: str = config.SUPPORTED_AI_MODELS[0],
+        init_datetime: datetime.datetime = datetime.datetime(2023, 7, 1, 0, 0),
+    ) -> None:
+        self.model_name = model_name
+        self.init_datetime = init_datetime
+        self.out_pth = config.make_output_path(model_name, init_datetime)
+        self.out_pth.parent.mkdir(parents=True, exist_ok=True)
+        logger.info(f"   Run initialization datetime: {self.init_datetime}")
+        logger.info(f"   Model output path: {str(self.out_pth)}")
+        self.init_model = model.load_model(
+            # Necessary arguments to instantiate a Model object
+            input="cds",
+            output="file",
+            download_assets=False,
+            name=self.model_name,
+            # Additional arguments. These are generally set as object attributes
+            # which are then referred to by various Model methods; unfortunately,
+            # they're not clearly declared in the class documentation so there is
+            # a bit of trial and error involved in figuring out what's needed.
+            assets=config.AI_MODEL_ASSETS_DIR,
+            date=int(self.init_datetime.strftime("%Y%m%d")),
+            time=self.init_datetime.hour,
+            lead_time=12,
+            path=str(self.out_pth),
+            metadata={},  # Read by the output data handler
+            # Unused arguments that are required by Model class methods to work.
+            model_args={},
+            assets_sub_directory=None,
+            staging_dates=None,
+            archive_requests=False,
+            only_gpu=True,
+        )
+
+    @modal.method()
+    def run_model(self) -> None:
+        self.init_model.run()
+
+
 @stub.function(
     image=stub.image,
     secret=config.ENV_SECRETS,
     network_file_systems={str(config.CACHE_DIR): volume},
-    gpu=config.DEFAULT_GPU_CONFIG,
-    timeout=600,
+    allow_cross_region_volumes=True,
 )
 def generate_forecast(
     model_name: str = config.SUPPORTED_AI_MODELS[0],
@@ -67,40 +115,15 @@ def generate_forecast(
 ):
     """Generate a forecast using the specified model."""
     logger.info(f"Attempting to initialize model {model_name}...")
-    logger.info(f"   Run initialization datetime: {init_datetime}")
-    out_pth = config.make_output_path(model_name, init_datetime)
-    out_pth.parent.mkdir(parents=True, exist_ok=True)
-    logger.info(f"   Model output path: {str(out_pth)}")
-    init_model = model.load_model(
-        # Necessary arguments to instantiate a Model object
-        input="cds",
-        output="file",
-        download_assets=False,
-        name=model_name,
-        # Additional arguments. These are generally set as object attributes
-        # which are then referred to by various Model methods; unfortunately,
-        # they're not clearly declared in the class documentation so there is
-        # a bit of trial and error involved in figuring out what's needed.
-        assets=config.AI_MODEL_ASSETS_DIR,
-        date=int(init_datetime.strftime("%Y%m%d")),
-        time=init_datetime.hour,
-        lead_time=12,
-        path=str(out_pth),
-        metadata={},  # Read by the output data handler
-        # Unused arguments that are required by Model class methods to work.
-        model_args={},
-        assets_sub_directory=None,
-        staging_dates=None,
-        archive_requests=False,
-        only_gpu=True,  # TODO: change this to True once we run on GPUs
-    )
+    ai_model = AIModel(model_name, init_datetime)
+
     logger.info("Generating forecast...")
-    init_model.run()
+    ai_model.run_model.remote()
     logger.info("Done!")
 
     # Double check that we successfully produced a model output file.
-    logger.info(f"Checking output file {str(out_pth)}...")
-    if out_pth.exists():
+    logger.info(f"Checking output file {str(ai_model.out_pth)}...")
+    if ai_model.out_pth.exists():
         logger.info("   Success!")
     else:
         logger.info("   Did not find expected output file.")
@@ -109,4 +132,4 @@ def generate_forecast(
 @stub.local_entrypoint()
 def main():
     check_assets.remote()
-    # generate_forecast.remote()
+    generate_forecast.remote()
